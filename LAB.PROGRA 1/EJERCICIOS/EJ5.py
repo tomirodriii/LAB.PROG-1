3c789252b4774a17f5852fdd42be081af8467d4a
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-12 11:40:02 -0300

    EJ5

--- LAB.PROGRA 1/EJERCICIOS/EJ5.py
+++ LAB.PROGRA 1/EJERCICIOS/EJ5.py
@@ -1,2 +1,10 @@
 # 5. Escribe un programa que compare dos números y muestre si son iguales o no utilizando operadores 
 # de comparación (por ejemplo, ==, !=)
+
+num1 = int(input("Ingrese un numero:"))
+num2 = int(input("Ingrese otro numero:"))
+
+if num1 == num2:
+    print(f"El numero {num1} es igual al {num2}")
+else:
+    print(f"El numero {num1} es diferente al {num2}")
