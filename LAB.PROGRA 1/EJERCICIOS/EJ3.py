15990ff98b7fc1c050afdc1fdf0905ffccce989c
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-03-12 11:41:11 -0300

    Correcion EJ3

--- LAB.PROGRA 1/EJERCICIOS/EJ3.py
+++ LAB.PROGRA 1/EJERCICIOS/EJ3.py
@@ -1,8 +1,8 @@
 #3. Escribe un programa que realice operaciones aritméticas simples, 
 #como suma, resta, multiplicación y división, utilizando números enteros y flotantes.
 
-num1 = int(input("Ingrese un numero"))
-num2 = int(input("Ingrese otro numero"))
+num1 = int(input("Ingrese un numero:"))
+num2 = int(input("Ingrese otro numero:"))
 
 sumados = num1 + num2
 restados = num1 - num2
@@ -10,6 +10,6 @@ multiplicados = num1 * num2
 divididos = num1 / num2
 
 print(f"Esos numeros sumados dan: {sumados}")
-print(f"Esos numeros sumados dan: {restados}")
-print(f"Esos numeros sumados dan: {multiplicados}")
-print(f"Esos numeros sumados dan: {divididos}")
+print(f"Esos numeros restados dan: {restados}")
+print(f"Esos numeros multiplicados dan: {multiplicados}")
+print(f"Esos numeros divididos dan: {divididos}")
